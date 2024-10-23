9107678c33655e890876fcf75facb5c1182d3963
Davlo-hub <oduor1606@gmail.com>
2024-10-23 13:18:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.qris = "https://a.top4top.io/p_319465f4i0.jpeg"
 
 
 // GLOBAL SETTING
-global.owner = "27623649420" //masukin no lu
+global.owner = "254111687009" //masukin no lu
 global.namabot = "Re-Jeong V4"
-global.nomorbot = "27623649420" //masukin no lu
+global.nomorbot = "254104260236" //masukin no lu
 global.namaCreator = "Re-Jeong "
 global.linkyt = "-"
 global.autoJoin = false
